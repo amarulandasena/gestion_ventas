64d62cb4b7089335361438c9c8b1982a9b0fa6fc
Andres_Marulanda <afmsamsung1981@gmail.com>
2025-06-25 20:50:27 -0500

    Se crean la pagina del login y el pie de página

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -6,18 +6,20 @@ import { Routes, Route } from 'react-router-dom'
 
 import { Login } from './Login/Login';
 import { PaginaPrincipal } from './PaginaPrincipal/PaginaPrincipal';
+import { PieDePagina } from './PieDePagina/PieDePagina';
 
 function App() {
   
   return (
     <>
-      <h1>APLICACIÓN PARA LA GESTIÓN DE VENTAS DE UNA MI PYMES</h1>
-
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='/' element={<Login
+          nombreEmpresa='nombreEmpresa.png' />} />
 
         <Route path='/paginaPrincipal' element={<PaginaPrincipal />} />
       </Routes>
+
+      <PieDePagina />
     </>
   )
 }
---
+++ Front/src/PieDePagina/PieDePagina.jsx
@@ -0,0 +1,13 @@
+import './PieDePagina.css';
+
+export const PieDePagina = () => {
+  return (
+    <footer className = "container-fluid">
+      <div className = "row pieDePagina">
+        <p className = "col-12 col-md-5">Todos los derechos reservados</p>
+        <p className = "col-12 col-md-4">Sabaneta, Antioquia, 2025</p>
+        <p className = "col-12 col-md-3">A&D Soft</p>
+      </div>   
+    </footer>
+  )
+}
